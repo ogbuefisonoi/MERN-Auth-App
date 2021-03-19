e6061fac31a159edc5f13845521c80aba5df61cc
ogbuefisonoi <80144390+ogbuefisonoi@users.noreply.github.com>
2021-03-19 19:56:11 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import CreateUser from './components/create-user';
-import EditUser from './components/edit-user';
-import UserList from './components/user-list';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from "axios";
+import Header from "./components/header";
+import Home from "./components/home";
+import Profile from "./components/profile";
+import Login from "./components/login";
+import Register from "./components/register";
+import UserContext from "./context/userContext";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">MERN-Stack Signup/Login App</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Userlist</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create User</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={UserList} />
-          <Route path="/edit/:id" component={EditUser} />
-          <Route path="/create" component={CreateUser} />
-        </div>
-      </Router>
-    );
-  }
+export default function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:5000/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenRes.data) {
+        const userRes = await Axios.get("http://localhost:5000/users/", {
+          headers: { "x-auth-token": token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
+  return (
+    <>
+      <BrowserRouter>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/profile" component={Profile} />
+            </Switch>
+          </div>
+        </UserContext.Provider>
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App;
